44922df90fe5e67d7a2689ea0b81f53ca83a03c5
Sezonov Danylo <prizrak7p@gmail.com>
2024-02-06 11:42:07 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,11 @@ def faktorial(a):
         return 1
     else:
         return a*faktorial(a-1)
+def five(b):
+    if b<1:
+        return False
+    else:
+        if b ==5:
+            return True
+        else:
+            return five(b/5)
